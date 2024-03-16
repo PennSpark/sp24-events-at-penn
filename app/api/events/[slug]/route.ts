65e83685594539a164e17e1693388c205917c1b2
luke-rt <luke.rtong@gmail.com>
2024-03-15 21:32:35 -0400

    url params

--- app/api/events/[slug]/route.ts
+++ app/api/events/[slug]/route.ts
@@ -3,24 +3,19 @@ import { getFirestore, collection, doc, getDoc } from "firebase/firestore";
 
 const db = getFirestore(FirebaseApp);
 
-export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-    const slug = searchParams.get("slug");
+export async function GET(request: Request, { params }: { params: { slug: string } }) {
+    const docRef = doc(db, "events", params.slug);
+    const docSnap = await getDoc(docRef);
 
-    if(slug) {
-        const docRef = doc(db, "events", slug);
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-            return Response.json({
-                status: 200,
-                body: docSnap.data(),
-            });
-        } else {
-            return Response.json({
-                status: 400,
-                body: `No event with slug '${slug}' was found.`,
-            });
-        }
-    }   
+    if (docSnap.exists()) {
+        return Response.json({
+            status: 200,
+            body: docSnap.data(),
+        });
+    } else {
+        return Response.json({
+            status: 400,
+            body: `No event with slug '${params.slug}' was found.`,
+        });
+    }
 }
