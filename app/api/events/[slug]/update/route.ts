cf35b9508bd20fe89834d93bc5819a054612dd33
Michael <limichael909@gmail.com>
2024-03-15 21:48:26 -0400

    post

--- app/api/events/[slug]/update/route.ts
+++ app/api/events/[slug]/update/route.ts
@@ -1,26 +1,43 @@
 import FirebaseApp from "../../../../lib/firebase";
-import { getFirestore, collection, doc, getDoc } from "firebase/firestore";
+import { getFirestore, collection, doc, getDoc, updateDoc } from "firebase/firestore";
+import { useSearchParams } from 'next/navigation';
 
 const db = getFirestore(FirebaseApp);
 
-export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-    const slug = searchParams.get("slug");
+export async function GET(request: Request, { params }: { params: { slug: string } }) {
+    const docRef = doc(db, "events", params.slug);
+    const docSnap = await getDoc(docRef);
 
-    if(slug) {
-        const docRef = doc(db, "events", slug);
-        const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        return Response.json({
+            status: 200,
+            body: docSnap.data(),
+        });
+    } else {
+        return Response.json({
+            status: 400,
+            body: `No event with slug '${params.slug}' was found.`,
+        });
+    }
+}
 
-        if (docSnap.exists()) {
-            return Response.json({
-                status: 200,
-                body: docSnap.data(),
-            });
-        } else {
-            return Response.json({
-                status: 400,
-                body: `No event with slug '${slug}' was found.`,
-            });
-        }
-    }   
+export async function POST(request: Request, { params }: { params: { slug: string } }) {
+    const docRef = doc(db, "events", params.slug);
+
+    const searchParams = useSearchParams();
+    const entries = searchParams.entries();
+
+    await updateDoc(docRef, {
+        name: "new name"
+    }).then(() =>  {
+        return Response.json({
+            status: 200,
+            body: entries,
+        });
+    }).catch(() => {
+        return Response.json({
+            status: 400,
+            body: `No event with slug '${params.slug}' was found.`,
+        });
+    })
 }
