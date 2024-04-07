35f6efd5d1c2ea42833c1ad5e9f032fdd4d668ee
Michael <limichael909@gmail.com>
2024-04-07 15:50:48 -0400

    event page template updated

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,52 +1,28 @@
 import Image from "next/image";
 
-const images = Array(6).fill(0);
+const images = Array(16).fill(0);
 
 export default function Events({ params }: { params: { slug: string } }) {
     return (
-        <>
-            <div className = "h-full">
+    
+        <div className = "min-h-screen h-fit mx-24 mt-10">
 
-            <div className = "grid grid-cols-2 gap-4 place-content-center h-48">
-                <div>
-                    <p className = "text-3xl font-extrabold">
-                        Event Title
-                        {params.slug}
-                    </p>
-
-                    <p>Organizer</p>
-                    <p>Time</p>
-                    <p>Tag(s)</p>
-                    <p>Location</p>
-                    <p>Registration</p>
-                    <p>Description</p>
-                </div>
-                
-                <div>
-                    <Image
-                        src = "next.svg"
-                        alt = "dog"
-                        height = {30}
-                        width = {30}
-                        
-                    />
-                </div>
+            <p className = "font-bold text-5xl">Explore the SocialCalendar@Penn!</p>
+            <p className = "font-semibold text-xl mt-2">Take a look at what's going on today</p>
+        
+            <div className = "grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] auto-rows-[170px] grid-flow-dense gap-5 my-20">
+                {images.map((img, index) => (
+                    <div>
+                        <Image
+                            src = "https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?cs=srgb&dl=pexels-pixabay-47547.jpg&fm=jpg"
+                            alt = "event"
+                            height = {200}
+                            width = {200}   
+                        />
+                    </div>
+                ))}
             </div>
-                
-            <p className = "text-2xl font-bold">Image Gallery</p>
-            
-            {images.map((img, index) => (
-                <>
-                    <Image
-                        src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Labrador_Retriever_portrait.jpg/1200px-Labrador_Retriever_portrait.jpg"
-                        alt = "dog"
-                        height = {400}
-                        width = {400}   
-                    />
-                </>
-            ))} 
-            
-            </div>
-        </>
+        </div>
+    
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
       <div className = "min-h-screen h-fit place-content-center grid justify-items-center">
           <p className = "font-extrabold text-6xl mt-[-20%]">SocialCalendar@Penn</p>
-          <p className = "font-extrabold text-2xl underline underline-offset-8 bg-yellow-200" 
-            style = {{
-              transform: "rotate(-4deg)",
-              textDecoration: "underline 3px"
-            }}>
-            Explore!
-          </p>
-       
+          <Link href="/events">
+            <p className = "font-extrabold text-2xl underline underline-offset-8 bg-yellow-200" 
+              style = {{
+                transform: "rotate(-4deg)",
+                textDecoration: "underline 3px"
+              }}>
+              Explore!
+            </p>
+          </Link>
         
       </div>
     </>
