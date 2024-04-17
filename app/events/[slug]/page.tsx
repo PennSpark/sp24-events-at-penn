ba2112f0cc39bc3388aec2ebd7c4cccfea2ab931
Michael <limichael909@gmail.com>
2024-04-17 14:13:43 -0400

    font changes on event page

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+/*import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+    
+})*/
 
 const images = Array(6).fill(0);
 
@@ -15,31 +20,31 @@ export default function Event({ params }: { params: { slug: string } }) {
                 <table className="table-auto text-lg mt-5 max-w-[650px]">
                     <tbody>
                         <tr>
-                            <td className = "text-gray-700 pr-5 align-top">Organizer</td>
+                            <td className = "montserrat pr-5 align-top">Organizer</td>
                             <td className = "pb-2 text-[#15009A] font-bold"><Link href = "/organizers">Organizer 1</Link></td>
                         </tr>
                         <tr>
-                            <td className = "text-gray-700 pr-5 align-top">Time</td>
+                            <td className = "montserrat text-gray-700 pr-5 align-top">Time</td>
                             <td className = "pb-2">3/17 6-7pm</td>
                         </tr>
                         <tr>
-                            <td className = "text-gray-700 pr-5 align-top">Tag(s)</td>
+                            <td className = "montserrat text-gray-700 pr-5 align-top">Tag(s)</td>
                             <td className = "pb-2">Sports</td>
                         </tr>
                         <tr>
-                            <td className = "text-gray-700 pr-5 align-top">Location</td>
+                            <td className = "montserrat text-gray-700 pr-5 align-top">Location</td>
                             <td className = "pb-2">Annenberg Center</td>
                         </tr>
                         <tr>
-                            <td className = "text-gray-700 pr-5 align-top">Address</td>
+                            <td className = "montserrat text-gray-700 pr-5 align-top">Address</td>
                             <td className = "pb-2">1234 Market Street</td>
                         </tr>
                         <tr>
-                            <td className = "text-gray-700 pr-5 align-top">Registration</td>
+                            <td className = "montserrat text-gray-700 pr-5 align-top">Registration</td>
                             <td className = "pb-2 text-[#15009A]"><Link href = "youtube.com">youtube.com</Link></td>
                         </tr>
                         <tr>
-                            <td className = "text-gray-700 pr-5 align-top">Description</td>
+                            <td className = "montserrat text-gray-700 pr-5 align-top">Description</td>
                             <td className = "pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur venenatis, enim quis scelerisque placerat, justo leo fringilla lacus, et lobortis urna purus a nunc. Quisque facilisis ipsum ornare metus accumsan congue. Proin cursus justo ut condimentum pellentesque. Morbi sollicitudin elit eget metus eleifend suscipit. Cras ipsum tortor, bibendum eget mollis id, bibendum non justo. In vel bibendum mauris. Suspendisse lacinia non nibh sed consequat.</td>
                         </tr>
                     </tbody>
@@ -68,15 +73,14 @@ export default function Event({ params }: { params: { slug: string } }) {
             
             <div className = "flex gap-5 overflow-x-scroll my-8">
                 {images.map((img, index) => (
-                    <>
-                        <Image
-                            src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Labrador_Retriever_portrait.jpg/1200px-Labrador_Retriever_portrait.jpg"
-                            alt = "dog"
-                            height = {400}
-                            width = {400} 
-                            style = {{objectFit: "cover", borderRadius: "12px"}}  
-                        />
-                    </>
+                    <Image
+                        src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Labrador_Retriever_portrait.jpg/1200px-Labrador_Retriever_portrait.jpg"
+                        alt = "dog"
+                        height = {400}
+                        width = {400} 
+                        style = {{objectFit: "cover", borderRadius: "12px"}}  
+                        key = {index}
+                    />
                 ))}
             </div>
         </div>
@@ -86,15 +90,14 @@ export default function Event({ params }: { params: { slug: string } }) {
             
             <div className = "flex gap-5 overflow-x-scroll my-8">
                 {images.map((img, index) => (
-                    <>
-                        <Image
-                            src = "https://cdn.pixabay.com/photo/2016/12/13/22/25/bird-1905255_1280.jpg"
-                            alt = "bird"
-                            height = {400}
-                            width = {400}   
-                            style = {{objectFit: "cover", borderRadius: "12px"}}
-                        />
-                    </>
+                    <Image
+                        src = "https://cdn.pixabay.com/photo/2016/12/13/22/25/bird-1905255_1280.jpg"
+                        alt = "bird"
+                        height = {400}
+                        width = {400}   
+                        style = {{objectFit: "cover", borderRadius: "12px"}}
+                        key = {index}
+                    />
                 ))}
             </div>
             
