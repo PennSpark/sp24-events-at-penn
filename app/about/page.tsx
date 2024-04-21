dd00c423867d4fac9c420cdb1993730ca06d79b7
Fiona Jiang <fionajiang812@gmail.com>
2024-04-21 15:40:09 -0400

    update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,24 +8,25 @@ export default function About() {
                 <h1 className = 'montserratStroke' >Welcome to SocialCalendar@Penn!</h1>
                 <h2 className = 'montserratStroke' >WHAT IS SocialCalendar@PENN?</h2>
                 <p>
-                SocialCalendar@Penn is a “social calendar” that would serve as a one-stop shop for Penn students to find promotional/exclusive events at our university. We aspire not only to offer Penn students a convenient overview of promotions near them but also to assist on-campus organizations in gaining more visibility.          </p>
+                Looking for something fun to do during the weekend? SocialCalendar@Penn is a one-stop shop for Penn students to find out social events. The platform not only offers Penn students a convenient overview of what is happening around the campus but also to assist organizations in gaining more visibility. With SocialCalendar, you will never miss another social gathering, promotional event, or party again.           </p>
              </div>
+
+             <div className = "for-whom">
+                <h2 className = 'montserratStroke' >FOR WHOM?</h2>
+                <p>
+                    All <b>off-campus social organizations</b> are welcomed to join us and promote your events here! From exclusive restaurant promotions to store discounts, themed parties to social gatherings, SocialCalendar@Penn covers it all.               
+                </p>
+            </div>
 
              <div className = "who-are-we">
                 <h2 className = 'montserratStroke' >WHO ARE WE?</h2>
                 <p>
-                Established in Spring 2021, Spark is a community of student developers and designers at the University of Pennsylvania that builds both creative and client projects. Find out more about us  
-                <a href="https://pennspark.org/"> here</a>
+                Established in Spring 2021, Spark is a community of student developers and designers at the University of Pennsylvania that builds both creative and client projects. Find out more about us <a href="https://pennspark.org/" className="underline-link">here</a>
                 !
                 </p>
             </div>
 
-            <div className = "for-whom">
-                <h2 className = 'montserratStroke' >FOR WHOM?</h2>
-                <p>
-                SocialCalendar@Penn is designed for University of Pennsylvania students seeking promotional/exclusive events and for on-campus organizations to publicize their activities. 
-Your events deserve to be noticed!                </p>
-            </div>
+            
 
                 <div className="about-rocket"></div>
                 <div className="about-smile"></div>
